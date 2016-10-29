260f11488bbdee932a70d776ed5161fec00c8ff5
David Fu <d23fu@uwaterloo.ca>
2016-10-28 20:58:11 -0400

    planet array

--- main.lua
+++ main.lua
@@ -1,10 +1,24 @@
 local Planet = require 'src.classes.planet'
 
+local planets = {}
+local delta
+
 function love.load()
+    for i = 1, 10 do
+        table.insert(planets, Planet(i * 50, i * 50, 10))
+    end
 end
 
 function love.update(dt)
+    delta = dt
+    for i = 1, 10 do
+        planets[i]:update(dt)
+    end
 end
 
 function love.draw()
+    love.graphics.print(delta, 5, 5)
+    for i = 1, 10 do
+        planets[i]:draw()
+    end
 end
--- src/classes/planet.lua
+++ src/classes/planet.lua
@@ -1,14 +1,18 @@
 local Class = require 'modules.hump.class'
+local Vector = require 'modules.hump.vector'
 
 local Planet = Class {}
 
-function Planet:init(x)
+function Planet:init(x, y, radius)
+    self.pos = Vector(x, y)
+    self.radius = radius
 end
 
 function Planet:update(dt)
 end
 
 function Planet:draw()
+    love.graphics.circle('line', self.pos.x, self.pos.y, self.radius)
 end
 
 return Planet
