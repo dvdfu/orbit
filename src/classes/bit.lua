281de879284ec1c441ebf74e212c402f21ceb15d
David Fu <d23fu@uwaterloo.ca>
2016-10-29 02:58:57 -0400

    little bits

---
+++ src/classes/bit.lua
@@ -0,0 +1,25 @@
+local Class = require 'modules.hump.class'
+local Vector = require 'modules.hump.vector'
+local Movable = require 'src.mixins.movable'
+
+local Bit = Class {
+    density = 1
+}
+Bit:include(Movable)
+
+function Bit:init(world, planets, x, y)
+    Movable.init(self, world, planets, x, y, 3)
+
+    self.body:setLinearDamping(0.5)
+    self.body:setAngularDamping(0.5)
+    self.fixture:setRestitution(0.9)
+
+    self.fixture:setUserData({
+        object = self,
+        tag = 'Bit',
+        collide = function(data) end,
+        endCollide = function(data) end
+    })
+end
+
+return Bit
--- src/classes/world.lua
+++ src/classes/world.lua
@@ -1,4 +1,5 @@
 local Class = require 'modules.hump.class'
+local Bit = require 'src.classes.bit'
 local Planet = require 'src.classes.planet'
 local Body = require 'src.mixins.body'
 
@@ -39,11 +40,18 @@ function World:init()
     self.physicsWorld:setCallbacks(beginContact, endContact, preSolve, postSolve)
 
     self.planets = {}
-    self:generatePlanets()
+    self.bits = {}
+
+    self:generate()
 end
 
 function World:update(dt)
     self.physicsWorld:update(dt)
+
+
+    for _, bit in pairs(self.bits) do
+        bit:update(dt)
+    end
 end
 
 function World:draw()
@@ -52,13 +60,23 @@ function World:draw()
     for _, planet in pairs(self.planets) do
         planet:draw()
     end
+
+    for _, bit in pairs(self.bits) do
+        bit:draw()
+    end
 end
 
-function World:generatePlanets()
+function World:generate()
     table.insert(self.planets, Planet(self.physicsWorld, 200, 200, 50))
     table.insert(self.planets, Planet(self.physicsWorld, 200, 400, 50))
     table.insert(self.planets, Planet(self.physicsWorld, 400, 200, 50))
     table.insert(self.planets, Planet(self.physicsWorld, 400, 400, 50))
+
+    for i = 1, 100 do
+        local bit = Bit(self.physicsWorld, self.planets, math.random(800), math.random(800))
+        bit.body:applyLinearImpulse(math.random(8), math.random(8))
+        table.insert(self.bits, bit)
+    end
 end
 
 return World
