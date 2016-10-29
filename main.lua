0f5e9bb32b2368a2454c0d3c62df6dfcde307b97
Hamdan Javeed <hamdan@hamdanjaveed.com>
2016-10-29 00:27:50 -0400

    dunno

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local M = require 'noise'
+
 function love.load()
 end
 
@@ -5,4 +7,11 @@ function love.update(dt)
 end
 
 function love.draw()
+    for i = 1, 100 do
+        for j = 1, 100 do
+            local a = M.Simplex2D(i, j)
+            love.graphics.setColor(a * 255, a * 255, a * 255)
+            love.graphics.points(i, j)
+        end
+    end
 end
