0b3ed4a23e74dd65c78ded05079559a103a001fa
Shrey Khosla <theshreyk@gmail.com>
2016-10-30 15:16:15 -0400

    Changed someting

--- src/classes/bit.lua
+++ src/classes/bit.lua
@@ -22,28 +22,18 @@ function Bit:init(world, planets, owner, x, y)
         object = self,
         tag = 'Bit',
         collide = function(data)
+            if self.owner ~= 0 then
+              Bit.HIT_SOUND:play()
+            end
             if data.tag == 'Planet' then
-                if self.owner ~= 0 then
-                  Bit.HIT_SOUND:play()
-                end
                 self.owner = 0
             elseif data.tag == 'Moon' then
-                if self.owner ~= 0 then
-                  Bit.HIT_SOUND:play()
-                end
                 self.owner = 0
             elseif data.tag == 'Sun' then
-                Bit.HIT_SOUND:play()
                 self.dead = true
             elseif data.tag == 'Asteroid' then
-                if self.owner ~= 0 then
-                  Bit.HIT_SOUND:play()
-                end
                 data.object.dead = true
             elseif data.tag == 'Bit' then
-                if self.owner ~= 0 then
-                  Bit.HIT_SOUND:play()
-                end
                 self.owner = 0
                 data.object.owner = 0
             end
